f2f5ba80a8e109dbba201aae262da65b0c882672
Daviti Didbaridze <ddidbaridze@gmail.com>
2023-02-08 18:41:08 +0100

    add context to components

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,18 +1,28 @@
 import React from 'react'
+import { useAppState } from './AppStateContext'
+
+import { Card } from './Card'
 import { AddNewItem } from './AddNewItem'
 import { ColumnContainer, ColumnTitle } from './styles'
 
 type ColumnProps = {
   text: string
-  children?: React.ReactNode
+  index: number
 }
 
-export const Column: React.FC<ColumnProps> = ({ text, children }) => {
+export const Column: React.FC<ColumnProps> = ({ text, index }) => {
+  const { state } = useAppState()
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
-      {children}
-      <AddNewItem toggleButtonText='+ Add another task' onAdd={console.log} dark />
+      {state.lists[index].tasks.map((task) => (
+        <Card text={task.text} key={task.id} />
+      ))}
+      <AddNewItem
+        toggleButtonText='+ Add another task'
+        onAdd={console.log}
+        dark
+      />
     </ColumnContainer>
   )
 }
